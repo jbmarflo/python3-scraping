3dbcfe8ce9470cb50449d00d9c617da2b41d23ff
joseph <jbmarflo@gmail.com>
2018-01-12 18:00:32 -0500

    pruebas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib import request, parse
-#from bs4 import BeautifulSoup
 #quote_page = input("Ingresa la pagina web:")
 
 page = requests.post('https://enlinea.sunedu.gob.pe/programa')
@@ -9,12 +8,17 @@ page = requests.post('https://enlinea.sunedu.gob.pe/programa')
 soup = BeautifulSoup(page.content, 'html.parser')
 
 list_data = soup.find_all('tr')
-
+datos={}
+count = 0
 for data in list_data:
-    td_list = data.find_all('td')
-    for td_data in td_list:
-    	print(td_data.get_text().encode('utf-8'))
-    print('\n')
+	count++	
+	td_list = data.find_all('td')
+	#datos[codigo] = td_list[0].get_text().encode('utf-8')
+	datos[institucion_nombre[count]] = ("df")
+	#datos[grado] = ("df")
+	#datos[carrera] = ("df")
+
+print(datos)
 #artist_name_list = soup.find(class_='BodyText')
 #artist_name_list_items = artist_name_list.find_all('a')
 #
